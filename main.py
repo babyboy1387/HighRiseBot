80f89cae5319ca0fe4cfc7e6c5538a5f69b422fa
babyboy1387 <vatanchiarmin0@gmail.com>
2025-01-18 21:15:31 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from asyncio import run as arun
 if __name__ == '__main__':
     path = "../token.txt"
     with open(path,"r",encoding='utf-8') as f:
-        token = f.readline().rstrip("\n")
-        room_id = f.readline().rstrip("\n")
+        token = f.readline().rstrip("050fa23e453578beb0321fbaa6d29e76dbd8b03148af11530a092aee86196c38")
+        room_id = f.readline().rstrip("660e6448818d797101c5d230")
     arun(Bot().run(room_id, token))
